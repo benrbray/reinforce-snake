b2be9e9e2d403c3c1976cdb55f86cae6ac9a0d40
Benjamin R. Bray <benrbray@gmail.com>
2025-08-14 22:08:05 +0900

    update

--- gym_snakegame/snake_game.py
+++ gym_snakegame/snake_game.py
@@ -5,45 +5,59 @@ import pygame
 import gymnasium as gym
 from gymnasium import spaces
 
+def compare(a, b):
+  if a == b: return 0;
+  if a > b:  return -1;
+  if a < b:  return 1;
+
 class SnakeGameEnv(gym.Env):
   metadata = {
     "render_modes": ["human", "rgb_array", "ansi"],
     "render_fps": 20,
   }
 
-  def __init__(self, render_mode=None, n_channel=1, board_size=15, n_target=1):
+  def __init__(self, render_mode=None, board_size=15, n_food=1):
     assert board_size >= 5
-    assert n_target > 0
-    assert n_channel in (1, 2, 4)
+    assert n_food > 0
 
+    # constants used to identify items on the game grid
     self.BLANK = 0
-    self.ITEM = board_size**2 + 1
     self.HEAD = 1
-    self.n_channel = n_channel
-
-    self.color_gradient = (255 - 100) / (board_size**2)
-
+    self.FOOD = board_size**2 + 1
+    
+    # in the original code, n_channel=(1, 2 or 4) but
+    # in this version, only a single channel is allowed
+    self.n_channel = 1
+    
+    # game state
     self.board_size = board_size  # The size of the square grid
+    self.n_food = n_food
+    self.snake = deque() # snake is represented by a list of (x,y) points 
+    self.board = np.zeros((self.board_size, self.board_size), dtype=np.uint32)
+    self.clock = None
+
+    # rendering data
+    self.window = None
+    self.render_mode = render_mode
+    self.color_gradient = (255 - 100) / (board_size**2)
     self.window_width = 600  # The size of the PyGame window
     self.window_height = 700
     self.window_diff = self.window_height - self.window_width
-    self.n_target = n_target
-    # space
-    self.observation_space = spaces.Box(
-      low=0,
-      high=self.ITEM,
-      shape=(self.n_channel, board_size, board_size),
-      dtype=np.uint32,
-    )
+    
+    # every gymnasium environment must observation and action spaces
+    
+    # Actions:  RIGHT, UP, LEFT, DOWN
     self.action_space = spaces.Discrete(4)
     self._action_to_direction = np.array([[1, 0], [0, 1], [-1, 0], [0, -1]])
-    # initialzie
-    self.snake = deque()
-    self.board = np.zeros((self.board_size, self.board_size), dtype=np.uint32)
-
-    self.render_mode = render_mode
-    self.window = None
-    self.clock = None
+    
+    # Observations:
+    # * 8 binary values, one per direction (N, NE, E, SE, S, SW, W, NW)
+    #     - True=Walkable, False=Blocked
+    # * 1 integer between [1,8] representing direction (N, NE, E, SE, S, SW, W, NW)
+    self.observation_space = spaces.Dict({
+      "vision": spaces.MultiBinary(n=(3,3)),
+      "smell": spaces.Box(low=-1, high=1, shape=(2,), dtype=np.int8),
+    })
 
   def reset(self, seed=None, options=None):
     super().reset(seed=seed)
@@ -73,44 +87,68 @@ class SnakeGameEnv(gym.Env):
   def _place_target(self, initial: bool = False) -> None:
     target_candidate = np.argwhere(self.board == self.BLANK)
     if initial:
-      target_list = target_candidate[self.np_random.choice(len(target_candidate), self.n_target)]
+      target_list = target_candidate[self.np_random.choice(len(target_candidate), self.n_food)]
       for x, y in target_list:
-        self.board[x, y] = self.ITEM
+        self.board[x, y] = self.FOOD
     else:
       if target_candidate.size == 0:
         return
       else:
         new_target = target_candidate[self.np_random.choice(len(target_candidate))]
-        self.board[new_target[0], new_target[1]] = self.ITEM
+        self.board[new_target[0], new_target[1]] = self.FOOD
 
   def _get_obs(self):
-    if self.n_channel == 1:
-      return self.board[np.newaxis, :, :]
-    else:
-      return self._split_channel(self.n_channel)
+    """
+    Use the current game state to construct an `observation` for the agent.
+    Should agree with the format specified by `self.observation_space`.
+    """
+    
+    # create a 3x3 binary array centered on the snake's head
+    vision = np.ones((3,3), dtype=np.bool)
+    head_pos = self.snake[-1]
+    head_c, head_r = head_pos;
+    
+    # loop over all 8 grid cells around the snake's head
+    for dr in [-1, 0, 1]:
+      for dc in [-1, 0, 1]:
+        board_c = head_c + dc;
+        board_r = head_r + dr;
+        
+        is_inside_grid = (
+          board_c >= 0              and
+          board_r >= 0              and
+          board_c < self.board_size and
+          board_r < self.board_size
+        );
+        
+        if is_inside_grid:
+          board_value = self.board[board_c][board_r];
+          is_blank = (board_value == self.BLANK);
+          is_food  = (board_value == self.FOOD);
+          is_walkable = is_food or is_blank;
+          
+          vision[1+dc, 1+dr] = (1 if is_walkable else 0);
+        else:
+          vision[1+dc, 1+dr] = 0;
+    
+    # get coordinates of nearest food to snake's head,
+    # and compute a smell vector in that direction
+    food_pos = self.get_nearest_food(self.board, head_pos);
+    smell_c = compare(head_c, food_pos[0]);
+    smell_r = compare(head_r, food_pos[1]);
+    smell = np.array([smell_c, smell_r], dtype=np.int8);
+    
+    return {
+      "vision": vision,
+      "smell": smell
+    }
 
-  def _split_channel(self, n_channel):
-    if n_channel == 2:
-      mask = self.board == self.ITEM
-      snake_obs = np.where(mask, 0, self.board)
-      target_obs = np.where(mask, self.board, 0)
-      return np.array([snake_obs, target_obs])
-    # n_channel == 4
-    else:
-      channels = []
-      # body
-      mask = (1 < self.board) & (self.board < len(self.snake))
-      channel = np.where(mask, self.board, 0)
-      channels.append(channel)
+  def get_nearest_food(self, grid, head_pos):
+    foods = np.array(np.where(grid == self.FOOD)).T
+    foods_dist = np.pow(foods - head_pos, 2).sum(axis=1)
 
-      # head, tail, target
-      without_body = (1, len(self.snake), self.ITEM)
-      for element in without_body:
-        mask = self.board == element
-        channel = np.where(mask, self.board, 0)
-        channels.append(channel)
-
-      return np.array(channels)
+    nearest_food_idx = np.argmin(foods_dist)
+    return foods[nearest_food_idx]
 
   def _get_info(self):
     return {"snake_length": len(self.snake), "prev_action": self.prev_action}
@@ -133,7 +171,7 @@ class SnakeGameEnv(gym.Env):
       reward = -1
       terminated = True
     # hit the snake
-    elif 0 < self.board[next_head[0], next_head[1]] < self.ITEM:
+    elif 0 < self.board[next_head[0], next_head[1]] < self.FOOD:
       reward = -1
       terminated = True
     else:
@@ -144,7 +182,7 @@ class SnakeGameEnv(gym.Env):
         reward = 0
         terminated = False
       # target
-      # self.board[next_head[0], next_head[1]] == self.ITEM
+      # self.board[next_head[0], next_head[1]] == self.FOOD
       else:
         self._score += 1
         reward = 1
@@ -184,34 +222,49 @@ class SnakeGameEnv(gym.Env):
 
     if self.render_mode in {"ansi"}:
       return self._render_ansi()
+    
+  
+  def _render_char(self, c):
+    if   c == self.FOOD:  return "●"; # food
+    elif c == self.HEAD:  return "█"; # snake head
+    elif c == self.BLANK: return "░"; # empty space
+    else:                 return "█"; # snake body
+
+  def _render_ansi(self):
+    result = "";
+    result += f"[ Score {self._score} ]\n"
+
+    horiz_line = "-"*self.board_size + '\n';
+    result += horiz_line;
+
+    for r in range(self.board_size):
+      for c in range(self.board_size):
+        result += self._render_char(self.board[r][c])
+      result += "\n"
+    
+    result += horiz_line;
+    result += "\nVision:";
+    
+    obs = self._get_obs();
+    vision = obs["vision"];
+    for r in range(3):
+      result += "\n";
+      for c in range(3):
+        if vision[r][c] == 1: result += "░";
+        else:                 result += "█";
+    
+    return result;
+  
+  # ---- PYGAME RENDERING CODE BELOW ---- #
 
   def get_body_color(self, r: int, c: int):
     color = 255 - self.color_gradient * self.board[r][c]
     return (color, color, color)
   
-  def _render_ansi(self):
-    result = "";
-    result += f"[ Score {self._score} ]\n"
-
-    horiz = "-"*self.board_size + '\n';
-    result += horiz;
-
-    for r in range(self.board_size):
-      for c in range(self.board_size):
-        if self.board[r,c] == self.ITEM:
-          result += "●";
-        elif self.board[r,c] == self.HEAD:
-          result += "█";
-        elif self.board[r,c] == self.BLANK:
-          result += "░";
-        else:
-          result += "█"
-
-      result += "\n"
-    
-    result += horiz;
-
-    return result;
+  def close(self):
+    if self.window is not None:
+      pygame.display.quit()
+      pygame.quit()
 
   def _render_frame(self):
     pygame.font.init()
@@ -269,7 +322,7 @@ class SnakeGameEnv(gym.Env):
               self.square_size,
             ),
           )
-        elif self.board[r, c] == self.ITEM:
+        elif self.board[r, c] == self.FOOD:
           pygame.draw.rect(
             canvas,
             (0, 255, 0),
@@ -305,8 +358,3 @@ class SnakeGameEnv(gym.Env):
       self.clock.tick(self.metadata["render_fps"])
     else:  # rgb_array
       return np.transpose(np.array(pygame.surfarray.pixels3d(canvas)), axes=(1, 0, 2))
-
-  def close(self):
-    if self.window is not None:
-      pygame.display.quit()
-      pygame.quit()
